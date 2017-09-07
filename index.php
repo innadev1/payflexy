fcea8add92eada379e156a3e059be4c7722362b2
innadev1 <innad2201@gmail.com>
2017-09-07 17:49:34 +0300

    new

--- index.php
+++ index.php
@@ -14,9 +14,77 @@
 		<div>
 			<?php include 'assets/header.php'; ?>
 			
-			<div id="about"></div>
+			<div id="about">
+				<div class="info1">
+					<h1>We are a team of <br>
+					experienced professionals in <br>
+					the field of e-commerce</h1>
+					<p>
+					We offer payment solutions for various businesses in Europe, CIS and worldwide. 
+					Having a wide network of trustworthy partners, we are always ready to make a 
+					profitable offer for you and your business. We offer package solutions for 
+					clients with business activities of different risk levels, and ICO projects 
+					are not an exception. The choice of payment solution is an important decision 
+					for development of your business.We are proud of the fact that several ICO 
+					projects have chosen PAYFLEXY and already work with our payment solution. 
+					</p>
+				</div>
+				
+				<div class="info2">
+					<h1>So, what are the <br>
+					advantages of PAYFLEXY? </h1>
+					<p>
+					Today, most ICO projects accept payments only in crypto currencies. But 
+					the possibility to accept fiat money will significantly increase the number
+					of your investors – as not all of potential clients have possibility to 
+					transfer crypto currency. Thus, it is very important for any ICO project 
+					to provide investors with the choice of payment method: crypto currency 
+					or fiat money, payment card or bank transfer, e-wallet or by cash (for 
+					example, Western Union). All these payment methods are offered by PAYFLEXY. 
+					</p>
+				</div>
+			</div>
+			
+			<div id="what_you_get">
+				<h2>Working with us, you get:</h2>
+				
+				<div class="wrap">
+					<div><img src="img/1.png"></div>
+					<div><img src="img/2.png"></div>
+					<div><img src="img/3.png"></div>
+					<div><img src="img/4.png"></div>
+				</div>
+				
+				<p>All you need to do is to choose the payment method 
+				which is most convenient for you and fill in the form 
+				on our website. Our experienced specialists will be glad
+				to consult you and to assist you in integration of the 
+				payment solution on your website for realization of your
+				ideas, which is indeed the base of any ICO project, in 
+				shortest possible time.</p>
+			</div>
 			
 			<div id="second_wave"></div>
+			
+			<div id="payment">
+				<h2>CARD PAYMENTS</h2>
+				
+				<div class="flex_button">
+					<div><button>VISA and MasterCard</button></div>
+					<div><button>Multi-currency processing</button></div>
+					<div><button>Card verification</button></div>
+					<div><button>Anti-fraud module</button></div>
+					<div><button>Payments monitoring departments</button></div>
+				</div>
+				
+				<div style="text-align: center; height: 30vw">KARTOČKA S INFOJ</div>
+				
+				<div class="wrap2">
+					<div>1</div>
+					<div>2</div>
+					<div>3</div>
+				</div>
+			</div>
 			
 			<?php include 'assets/footer.php'; ?>
 
