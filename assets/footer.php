b3cc909b25ab453aea0ab9b7537d04c9e1249a12
WillSylvan <silvis.silvestrs@gmail.com>
2017-09-08 11:59:59 +0300

    form2 subscribe

--- assets/footer.php
+++ assets/footer.php
@@ -2,59 +2,40 @@
 	include ('assets/connect.php');
 	include ('assets/function.php');
 
-    if(isset($_POST['sent']))
+    if(isset($_POST['SUBMIT']))
     {
 
 		$email = $_POST['email'];
 		$phone = $_POST['phone'];
 
-        $row = getDataFromDatabase("SELECT email FROM emails WHERE email=:email", [ 
-            'email' => $email,
-        ]);
+		$email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+		
+		if(!preg_match($email_exp,$email)) {
+			echo "<script>alert('kluuda');</script>";
 
-        $emailErr = '';
-
-        if($row['email']==$email) {
-            $emailError = "Your e-mails are already there!".$row['email'];
-            $trueError = true;
-            $emailErr = 'emailErr'.$row['email'];
-
-        }else{
-
-            $sql = "INSERT INTO emails (email, phone) VALUES(:email, :phone)";
-    
-            $row = insertDataInToDataBase($sql, [
+		}else{
+		
+			$row = getDataFromDatabase("SELECT email FROM emails WHERE email=:email", [ 
 				'email' => $email,
-				'phone' => $phone,
-			]);
-			
-			$sql = "INSERT INTO emails (phone) VALUES(:phone)";
-			
-			$row = insertDataInToDataBase($sql, [
-				'phone' => $phone
 			]);
 
-			$email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-			
-				if(!preg_match($email_exp,$email)) {
-					echo "<script>alert('kluuda');</script>";
-					// $error_message_em .= '<p style = "color: red;">Please enter email!</p>';
-					// $errors['email'] = 1;
-				}
-		}
-			if(isset($emailError)){
-				// alert ($emailError);
+			if($row['email']==$email) {
 
 				echo "<script>alert(' Your e-mails are already there!');</script>";
+			
 			}else{
 
-				echo "<script>alert('Your email is registered!');</script>";
-
+				$sql = "INSERT INTO emails (email, phone) VALUES(:email, :phone)";
+		
+				$row = insertDataInToDataBase($sql, [
+					'email' => $email,
+					'phone' => $phone,
+				]);
+				echo "<script>alert('Your email is registered!');</script>";			
 			}
-						
-    	}else{
-
+		}
 	}
+	
 ?>
 <!-- hghghghghghggdegfdfgdfgdsadasda -->
 
@@ -70,25 +51,25 @@
 	<div class="flex">
 		<form action="index.php" method="post" name ="subForm" class="inputs" >
 			<div class="inputs">
-				<div><p>Phone number</p></div>
+				<div><p>Email</p></div>
 				<input class="footer_input" type="text" name="email"></input>
 			</div>
 
 			<div class="inputs">
-				<div><p>Email</p></div>
+				<div><p>Phone</p></div>
 				<input class="footer_input" type="text" name="phone"></input>
 				<!-- <button class="subscribe"> -->
 			</div>
 				
 			<!-- </button> -->
 			<div class="submit">
-				<input class="footer_button" type="submit" name="sent" value="SUBMIT"></input>
+				<input class="footer_button" type="submit" name="SUBMIT" value="SUBMIT"></input>
 			</div>
 		</form>
 			
 		
 		
-		<div class="button">
+		<div class="button" style="display: none">
 			<button>or click here</button>
 			<p>and fill the form below and one of our managers<br> will contact you as soon as possible.</p>
 		</div>
