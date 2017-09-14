ac6b58ea34048c60cd7c33f061ec2cd9e2228105
PavilsDDz <pavilsdzi@gmail.com>
2017-09-14 11:59:54 +0300

    Mae

--- index.php
+++ index.php
@@ -8,9 +8,8 @@
 <html>
     <head>   
 		<meta name="viewport" content="width=device-width, initial-scale=1">  
-		<link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Baloo" rel="stylesheet">
-		<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,700" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700&amp;subset=cyrillic-ext" rel="stylesheet">
 		<link rel="stylesheet" type="text/css" href="style/index.css">
 		<link rel="stylesheet" type="text/css" href="style/style.css">
         <title>payflexy</title>
